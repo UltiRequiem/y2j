6fe681f4d0f3e62f0d43f2361c0b02cf0c60020e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-05 09:15:14 -0500

    :art: Move Export to the end of the file

--- mod.ts
+++ mod.ts
@@ -1,7 +1,5 @@
 import { yamlParse, yamlStringify } from "./depts.ts";
 
-export { yamlParse as YAMLtoObject, yamlStringify as ObjecToYAML };
-
 export function JSONtoYAML(jsonString: string): string {
   return yamlStringify(JSON.parse(jsonString));
 }
@@ -9,3 +7,5 @@ export function JSONtoYAML(jsonString: string): string {
 export function YAMLtoJSON(yamlString: string): string {
   return JSON.stringify(yamlParse(yamlString), null, 2);
 }
+
+export { yamlParse as YAMLtoObject, yamlStringify as ObjecToYAML };
