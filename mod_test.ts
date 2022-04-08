81bca63184c92c157a83f7e9d94d6e19ffedf8af
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-08 09:39:22 -0500

    :white_check_mark: Add test

--- mod_test.ts
+++ mod_test.ts
@@ -1,2 +1,29 @@
-import {} from "./mod.ts";
 import { assertEquals } from "https://deno.land/std@0.134.0/testing/asserts.ts";
+
+import { JSONtoYAML, YAMLtoJSON } from "./mod.ts";
+
+const exampleJSON = `{
+  "name": "John",
+  "age": 30,
+  "cars": [
+    "Ford",
+    "BMW",
+    "Fiat"
+  ]
+}`;
+
+const exampleYAML = `name: John
+age: 30
+cars:
+  - Ford
+  - BMW
+  - Fiat
+`;
+
+Deno.test("JSON to YAML", () => {
+  assertEquals(JSONtoYAML(exampleJSON), exampleYAML);
+});
+
+Deno.test("YAML to JSON", () => {
+  assertEquals(YAMLtoJSON(exampleYAML), exampleJSON);
+});
