d4ff6ec0dac4322450a278241ac2768b914d5882
GitHub <noreply@github.com>
2023-02-01 00:18:44 +0000

    :arrow_up: update deno dependencies

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.170.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.175.0/testing/asserts.ts";
 
 import { JSONtoYAML, YAMLtoJSON } from "./mod.ts";
 
